23fba0e7110f1323e035e56aa0c3306ced0cc484
Marco <Marco@LAPTOP-A0R736MS.homenet.telecomitalia.it>
2020-05-23 11:19:56 +0200

    Versione corretta

--- src/main/java/it/polito/tdp/Emergency/model/TestSimulator.java
+++ src/main/java/it/polito/tdp/Emergency/model/TestSimulator.java
@@ -12,12 +12,12 @@ public class TestSimulator {
 		sim.init();
 		sim.run();
 		
-		System.out.println("** STATISTICHE **") ;
+		/*System.out.println("** STATISTICHE **") ;
 		System.out.format("Studi medici: %d\n", sim.getNS());
 		System.out.format("Pazienti:     %d\n", sim.getPazientiTot());
 		System.out.format("Dimessi:      %d\n", sim.getPazientiDimessi());
 		System.out.format("Morti:        %d\n", sim.getPazientiMorti());
-		System.out.format("Abbandonano:  %d\n", sim.getPazientiAbbandonano());
+		System.out.format("Abbandonano:  %d\n", sim.getPazientiAbbandonano());*/
 	}
 
 }
